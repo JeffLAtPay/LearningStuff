932fe72997b2ef03bc7ca2215335c404a4bd3934
Jeff L <jeffl@atpay.com>
2013-05-27 00:24:52 -0600

    Measure true progress.

--- TDD/1/app.rb
+++ TDD/1/app.rb
@@ -1,10 +1,4 @@
 require 'sinatra'
-require 'sinatra/activerecord'
-
-set :database, "sqlite3///blog.db"
-
-class Post < ActiveRecord::Base
-end
 
 def reverse string
   string.each_char.to_a.reverse.join
